75256787861f33a5aa9d68187db8f78e3972fd85
Nick Strydom <nstrydom@gmail.com>
2019-07-19 18:55:24 -0500

    Encapsulated Broker fields

--- src/main/java/org/bitnick/school/cs310progassn1/Broker.java
+++ src/main/java/org/bitnick/school/cs310progassn1/Broker.java
@@ -10,5 +10,83 @@ package org.bitnick.school.cs310progassn1;
  * @author Nicholas B. Strydom
  */
 public class Broker {
+    private String licenseNum;
+    private String firstName;
+    private String lastName;
+    private String deptNum;
+    private double commisionRate;
+    
     public Broker() {}
+
+    /**
+     * @return the licenseNum
+     */
+    public String getLicenseNum() {
+        return licenseNum;
+    }
+
+    /**
+     * @param licenseNum the licenseNum to set
+     */
+    public void setLicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the deptNum
+     */
+    public String getDeptNum() {
+        return deptNum;
+    }
+
+    /**
+     * @param deptNum the deptNum to set
+     */
+    public void setDeptNum(String deptNum) {
+        this.deptNum = deptNum;
+    }
+
+    /**
+     * @return the commisionRate
+     */
+    public double getCommisionRate() {
+        return commisionRate;
+    }
+
+    /**
+     * @param commisionRate the commisionRate to set
+     */
+    public void setCommisionRate(double commisionRate) {
+        this.commisionRate = commisionRate;
+    }
+    
+    
 }
