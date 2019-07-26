f5c88d55098dde8afb7e8e2070c1c40e68aa0c42
Nick Strydom <nstrydom@gmail.com>
2019-07-26 04:56:27 -0500

    Added file input

--- src/main/java/org/bitnick/school/cs310progassn1/CS310Lastname.java
+++ src/main/java/org/bitnick/school/cs310progassn1/CS310Lastname.java
@@ -5,10 +5,38 @@
  */
 package org.bitnick.school.cs310progassn1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
- * @author ghost
+ * @author Nicholas B. Strydom
  */
 public class CS310Lastname {
+    public static void setBrokerAttributes() {
+        
+    }
     
+    public static void displayBrokerAttributes() {
+        
+    }
+    
+    public static void setStockTradeAttributes() {
+        
+    }
+    
+    public static void displayStockTradeAttributes() {
+        
+    }
+    
+    public static void main(String[] args) {
+        final String INPUT_FILE_PATH = "/home/ghost/NetBeansProjects/CS310ProgAssn1/testData.csv";
+        
+        try (Scanner fileScanner = new Scanner(new File(INPUT_FILE_PATH))) {
+            
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
